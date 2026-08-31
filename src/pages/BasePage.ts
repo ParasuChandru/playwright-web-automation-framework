8042664655410f03293a6f15389c558940c06908
ParasuChandru <80024826+ParasuChandru@users.noreply.github.com>
2026-08-31 17:58:52 +0530

    Add BasePage

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -1,10 +1,12 @@
-import { Locator, Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
+import { env } from '@utils/env';
 
 export class BasePage {
   protected readonly page: Page;
 
   constructor(page: Page) {
     this.page = page;
+    this.page.setDefaultTimeout(env.defaultTimeout);
   }
 
   async navigate(path = ''): Promise<void> {
@@ -13,5 +15,13 @@ export class BasePage {
 
   locator(selector: string): Locator {
     return this.page.locator(selector);
+  }
+
+  async getPageTitle(): Promise<string> {
+    return this.page.title();
+  }
+
+  async verifyUrlContains(value: string): Promise<void> {
+    await expect(this.page).toHaveURL(new RegExp(value));
   }
 }
